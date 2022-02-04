99bef0dffc214b8b27275d60a78a13cc90cc9430
Tanya Lama <36007010+tanyalama@users.noreply.github.com>
2022-02-04 08:56:35 -0500

    Update sra_upload.sh

--- scripts/sra_upload.sh
+++ scripts/sra_upload.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
-
+  
 #
-#SBATCH --job-name=SRA
+#SBATCH --job-name=SRA-folder
 #SBATCH -o slurm-%j.out  # %j = job ID
 #SBATCH --ntasks-per-node=40
 #SBATCH --nodes=1
-#SBATCH --time=1:00:00
-#SBATCH -p gpu
+#SBATCH --time=96:00:00 #this took ~48h
+#SBATCH -p gpu-long
 
-ascp -i /home/tlama_umass_edu/.aspera/connect/etc/asperaweb_id_dsa.openssh -QT -l100m -k1 -d /home/tlama_umass_edu/scratch/project_canada_lynx_wgs/SRA_SUB11032410_canada_lynx_wgs.tar.gz subasp@upload.ncbi.nlm.nih.gov:uploads/tanya.m.lama_gmail.com_Z728gXCa
+ascp -i /home/tlama_umass_edu/.aspera/connect/etc/asperaweb_id_dsa.openssh -QT -l100m -k1 -d /home/tlama_umass_edu/scratch/project_canada_lynx_wgs/SRA_SUB11032410_canada_lynx_wgs subasp@upload.ncbi.nlm.nih.gov:uploads/tanya.m.lama_gmail.com_Z728gXCa
